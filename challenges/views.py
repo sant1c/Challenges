b805a64fe6d61060e9c299cfeb27bb511b167048
Nikhil <nykhilamatya@gmail.com>
2025-02-02 18:42:13 +1100

    update

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+
+monthly_challenges = {
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for atleast 20 minutes every day!",
+    "march": "Learn Django for atleast 20 minutes every day!",
+    "april": "Eat no meat for the entire month!",
+    "may": "Walk for atleast 20 minutes every day!",
+    "june": "Learn Django for atleast 20 minutes every day!",
+    "july": "Eat no meat for the entire month!",
+    "august": "Walk for atleast 20 minutes every day!",
+    "september": "Learn Django for atleast 20 minutes every day!",
+    "october": "Eat no meat for the entire month!",
+    "november": "Walk for atleast 20 minutes every day!",
+    "december": "Learn Django for atleast 20 minutes every day!"
+}
+
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+    
+    resonse_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(resonse_data)
+
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("Invalid Month")
+    redirect_month = months[month - 1]
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
+
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This month is not supported!")
